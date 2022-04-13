0082d31e81b3b7c8101369806c2711521045dbeb
Msksgm <msksgm3124@gmail.com>
2022-04-14 06:30:16 +0900

    add: userfactory を追加

---
+++ domain/model/user/userfactory_test.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+func Test_Create(t *testing.T) {
+	name, err := NewUserName("userName")
+	if err != nil {
+		t.Error(err)
+	}
+
+	userFactory, err := NewUserFactory()
+	if err != nil {
+		t.Error(err)
+	}
+
+	got, err := userFactory.Create(*name)
+	if err != nil {
+		t.Error(err)
+	}
+
+	want := &User{id: UserId{value: "id"}, name: *name}
+
+	if diff := cmp.Diff(want, got, cmp.AllowUnexported(User{}, UserId{}, UserName{}), cmpopts.IgnoreFields(*got, "id")); diff != "" {
+		t.Errorf("mismatch (-want, +got):\n%s", diff)
+	}
+}
